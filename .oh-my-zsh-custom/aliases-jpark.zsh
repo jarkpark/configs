b6829871a51c6cc1d48c352d5a8344c9f60edcf8
James Park <james.park@samba.tv>
2019-07-11 14:37:12 -0500

    Remove python/3 alias

--- .oh-my-zsh-custom/aliases-jpark.zsh
+++ .oh-my-zsh-custom/aliases-jpark.zsh
@@ -1,11 +1,6 @@
 # Custom Aliases
 # Author: James Park
 
-if [[ -f /usr/local/bin/python3 ]]; then
-	alias python="/usr/local/bin/python3"
-	alias python2="/usr/bin/python"
-fi
-
 if [[ -f $ZSH_CUSTOM/cheatsheet-jpark.sh ]]; then
 	alias hh="less $ZSH_CUSTOM/cheatsheet-jpark.sh"
 fi
