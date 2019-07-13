9a6a84ccca3bccfdfc9f7fa1493ca46486b39f5c
James Park <james.park@samba.tv>
2019-07-13 01:55:34 -0500

    Address TODOs, clearly organize each section

--- system_prefs.sh
+++ system_prefs.sh
@@ -1,45 +1,57 @@
 #!/bin/bash
 
-
+##################################################
 # System Preferences
-# Close System Preferences to prevent overriding settings we’re about to change
+##################################################
+# Close System Preferences to prevent overriding settings we're about to change
 osascript -e 'tell application "System Preferences" to quit'
 
+# Ask for the administrator password upfront
+sudo -v
 
+# Keep-alive: update existing 'sudo' time stamp until '.macos' has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+##################################################
 # General
+##################################################
 # TODO: Default web browser
 
 
+##################################################
 # Desktop & Screen Saver
+##################################################
 # TODO: Screen Saver > Star after: 10 Minutes
 # Hot Corners > Bottom Left = Put Display to Sleep
 defaults write com.apple.dock wvous-bl-corner -int 10
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 
+##################################################
 # Dock
-# Size:
+##################################################
+# Size
 defaults write com.apple.dock tilesize -int 60
 
-# Magnification:
+# Magnification
 defaults write com.apple.dock magnification -bool false
 
-# Automatically hide and show the Dock:
+# Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
-# Automatically hide and show the Dock (delay):
+# Automatically hide and show the Dock (delay)
 # Undo with: defaults delete com.apple.Dock autohide-delay
 defaults write com.apple.Dock autohide-delay -float 0
 
-# Automatically hide and show the Dock (duration):
+# Automatically hide and show the Dock (duration)
 # Undo with: defaults delete com.apple.Dock autohide-time-modifier
 defaults write com.apple.Dock autohide-time-modifier -float 0
 
-# Restart Dock to apply changes
-killall Dock
 
-
+##################################################
 # Mission Control
+##################################################
 # Automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
 
@@ -47,28 +59,38 @@ defaults write com.apple.dock mru-spaces -bool false
 defaults write com.apple.dock expose-group-apps -bool true
 
 
+##################################################
 # Security & Privacy
+##################################################
 # TODO: Require password immediately, Allow apps downloaded from identified developers
 
 
+##################################################
 # Display
+##################################################
 # TODO: True Tone (true), Night Shift > Schedule and color temp
 
 
+##################################################
 # Energy Saver
+##################################################
 # TODO: Battery > Enable power nap
 
 
+##################################################
 # Keyboard
-# TODO: Text > disable spell correction
+##################################################
 # Keyboard > Key Repeat
 defaults write NSGlobalDomain KeyRepeat 2
 
 # Keyboard > Delay Until Repeat
 defaults write NSGlobalDomain InitialKeyRepeat 15
 
-# Text > Clear Text Replacements
-#defaults write NSGlobalDomain NSUserDictionaryReplacementItems () ?
+# Keyboard > Touch Bar shows
+defaults write com.apple.touchbar.agent -string "fullControlStrip"
+
+# Text > Replace With Dictionary
+defaults write NSGlobalDomain NSUserDictionaryReplacementItems -array
 
 # Text > Correct spelling automatically
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
@@ -81,9 +103,16 @@ defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
 defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
 
 # Text > Touch Bar typing suggestions
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+
+# Text > Use smart quotes and dashes
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 
+##################################################
 # Trackpad
+##################################################
 # Point & Click > Click
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0
 defaults write com.apple.AppleMultitouchTrackpad SecondClickThreshold -int 0
@@ -92,9 +121,19 @@ defaults write com.apple.AppleMultitouchTrackpad SecondClickThreshold -int 0
 defaults write com.apple.dock showAppExposeGestureEnabled -bool true
 
 
+##################################################
 # Date & Time
+##################################################
 Clock > Show date
 defaults write com.apple.menuextra.clock DateFormat -string "EEE MMM d  h:mm a"
 defaults write com.apple.menuextra.clock FlashDateSeparators -bool false
 defaults write com.apple.menuextra.clock IsAnalog -bool false
+
+
+##################################################
+# Post-Setup Steps
+##################################################
+killall Dock
+killall Finder
+killall SystemUIServer
 
