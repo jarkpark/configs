8585db9042a4bd43f0825ba6e5b4d94136631d0a
James Park <james.park@samba.tv>
2019-08-08 11:45:54 -0500

    Add gi() function for opening git repo in browser

--- .oh-my-zsh-custom/functions-jpark.zsh
+++ .oh-my-zsh-custom/functions-jpark.zsh
@@ -10,6 +10,24 @@ togglewifi () {
 	echo "Toggled off then on!"
 }
 
+# Function to open current git repo in the browser
+# git@bitbucket.org:sambatv/recommendations-api.git
+# https://bitbucket.org/sambatv/recommendations-api/
+# git@github.com:jarkpark/dotfiles.git
+# https://github.com/jarkpark/dotfiles
+gi () {
+    giturl=$(git config --get remote.origin.url)
+    if [ -z "$giturl" ]
+    then
+        echo "Not a git repo or remote.origin.url not set!"
+        return
+    fi
+    giturl=${giturl/:/\/}
+    giturl=${giturl/git@/https:\/\/}
+    giturl=${giturl/.git/\/}
+    open $giturl
+}
+
 # Zsh hook functions
 function chpwd() {
     ls -lah
